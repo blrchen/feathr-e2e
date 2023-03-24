4f1d82e450540a8defa99df803eb77b79c004c26
徐辰 <windoze@0d0a.com>
2023-03-25 02:14:39 +0800

    Update training code

--- model/train.py
+++ model/train.py
@@ -1,109 +1,101 @@
-import pandas as pd  
-import numpy as np  
-import torch  
-import torch.nn as nn  
-from torch.utils.data import Dataset, DataLoader  
-  
-# Define dataset class  
-class MovieLensDataset(Dataset):  
-    def __init__(self, df):  
-        self.user_ids = df['userId'].values  
-        self.movie_ids = df['movieId'].values  
-        self.ratings = df['rating'].values  
-          
-    def __len__(self):  
-        return len(self.user_ids)  
-      
-    def __getitem__(self, idx):  
-        return (self.user_ids[idx], self.movie_ids[idx], self.ratings[idx])  
-      
-# Define model class  
-class Recommender(nn.Module):  
-    def __init__(self, n_users, n_movies, emb_size=50, hidden_size=100):  
-        super().__init__()  
-        self.user_emb = nn.Embedding(n_users, emb_size)  
-        self.movie_emb = nn.Embedding(n_movies, emb_size)  
-        self.fc1 = nn.Linear(2*emb_size, hidden_size)  
-        self.fc2 = nn.Linear(hidden_size, 1)  
-          
-    def forward(self, user_ids, movie_ids):  
-        user_vec = self.user_emb(user_ids)  
-        movie_vec = self.movie_emb(movie_ids)  
-        x = torch.cat([user_vec, movie_vec], dim=1)  
-        x = nn.functional.relu(self.fc1(x))  
-        x = self.fc2(x)  
-        return x.view(-1)  
-      
-# Define training function  
-def train(model, dataloader, optimizer, criterion, device):  
-    model.train()  
-    train_loss = 0.0  
-    for user_ids, movie_ids, ratings in dataloader:  
-        user_ids, movie_ids, ratings = user_ids.to(device), movie_ids.to(device), ratings.to(device)  
-        optimizer.zero_grad()  
-        outputs = model(user_ids, movie_ids)  
-        loss = criterion(outputs, ratings)  
-        loss.backward()  
-        optimizer.step()  
-        train_loss += loss.item() * len(user_ids)  
-    return train_loss / len(dataloader.dataset)  
-  
-# Define validation function  
-def validate(model, dataloader, criterion, device):  
-    model.eval()  
-    valid_loss = 0.0  
-    with torch.no_grad():  
-        for user_ids, movie_ids, ratings in dataloader:  
-            user_ids, movie_ids, ratings = user_ids.to(device), movie_ids.to(device), ratings.to(device)  
-            outputs = model(user_ids, movie_ids)  
-            loss = criterion(outputs, ratings)  
-            valid_loss += loss.item() * len(user_ids)  
-    return valid_loss / len(dataloader.dataset)  
-  
-# Save the trained model to a file  
-def save_model(model, file_path):  
-    torch.save(model.state_dict(), file_path)  
-  
-# Load the trained model from a file  
-def load_model(model, file_path):  
-    model.load_state_dict(torch.load(file_path))  
-  
-# Load CSV data  
-data = pd.read_csv('ratings.csv')  
-  
-# Map user and movie IDs to contiguous integers  
-user_ids = data['userId'].unique().tolist()  
-user2idx = {o:i for i,o in enumerate(user_ids)}  
-data['userId'] = data['userId'].apply(lambda x: user2idx[x])  
-  
-movie_ids = data['movieId'].unique().tolist()  
-movie2idx = {o:i for i,o in enumerate(movie_ids)}  
-data['movieId'] = data['movieId'].apply(lambda x: movie2idx[x])  
-  
-# Split data into train and validation sets  
-msk = np.random.rand(len(data)) < 0.8  
-train_data = data[msk]  
-valid_data = data[~msk]  
-  
-# Set up device  
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
-  
-# Set up dataset and dataloader  
-train_dataset = MovieLensDataset(train_data)  
-valid_dataset = MovieLensDataset(valid_data)  
-train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)  
-valid_dataloader = DataLoader(valid_dataset, batch_size=64)  
-  
-# Set up model, optimizer, and loss function  
-model = Recommender(len(user_ids), len(movie_ids)).to(device)  
-optimizer = torch.optim.Adam(model.parameters())  
-criterion = nn.MSELoss()  
-  
-# Train model  
-n_epochs = 20  
-for epoch in range(n_epochs):  
-    train_loss = train(model, train_dataloader, optimizer, criterion, device)  
-    valid_loss = validate(model, valid_dataloader, criterion, device)  
-    print(f'Epoch {epoch+1}, train_loss = {train_loss:.4f}, valid_loss = {valid_loss:.4f}')  
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-save_model(model, "recommendation_model.pth")  
+# Load dataset
+data = pd.read_csv('ratings_small.csv')
+
+# Preprocessing
+users = data['userId'].unique()
+movies = data['movieId'].unique()
+user_to_index = {user: idx for idx, user in enumerate(users)}
+movie_to_index = {movie: idx for idx, movie in enumerate(movies)}
+data['userId'] = data['userId'].apply(lambda x: user_to_index[x])
+data['movieId'] = data['movieId'].apply(lambda x: movie_to_index[x])
+
+# Split into training and validation sets
+train_data = data.sample(frac=0.8, random_state=1)
+val_data = data.drop(train_data.index)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class RecommenderSystem(nn.Module):
+    def __init__(self, n_users, n_movies, n_factors=20):
+        super().__init__()
+        self.user_factors = nn.Embedding(n_users, n_factors)
+        self.movie_factors = nn.Embedding(n_movies, n_factors)
+        self.user_biases = nn.Embedding(n_users, 1)
+        self.movie_biases = nn.Embedding(n_movies, 1)
+        
+    def forward(self, user, movie):
+        user_embedding = self.user_factors(user)
+        movie_embedding = self.movie_factors(movie)
+        dot_product = (user_embedding * movie_embedding).sum(dim=-1)
+        user_bias = self.user_biases(user).squeeze()
+        movie_bias = self.movie_biases(movie).squeeze()
+        return dot_product + user_bias + movie_bias
+
+def train_model(model, train_data, val_data, epochs=10, learning_rate=0.01, weight_decay=0.001):
+    model.to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    loss_function = nn.MSELoss()
+
+    for epoch in range(epochs):
+        model.train()
+        train_loss = 0
+
+        count = 0
+        for _, row in train_data.iterrows():
+            count += 1
+            if count % 10000 == 0:
+                print(f"{datetime.now()} Epoch {epoch+1}/{epochs}: {count}/{len(train_data)}")
+            user, movie, rating = int(row['userId']), int(row['movieId']), float(row['rating'])
+            optimizer.zero_grad()
+            prediction = model(torch.tensor(user).to(device), torch.tensor(movie).to(device))
+            loss = loss_function(prediction, torch.tensor(rating).to(device))
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+
+        model.eval()
+        val_loss = 0
+
+        with torch.no_grad():
+            for _, row in val_data.iterrows():
+                user, movie, rating = int(row['userId']), int(row['movieId']), float(row['rating'])
+                prediction = model(torch.tensor(user).to(device), torch.tensor(movie).to(device))
+                loss = loss_function(prediction, torch.tensor(rating).to(device))
+                val_loss += loss.item()
+
+        print(f"{datetime.now()} Epoch {epoch+1}/{epochs}: Train Loss: {train_loss/len(train_data)}, Val Loss: {val_loss/len(val_data)}")
+        torch.save(model.state_dict(), f"model_{epoch+1}.pth")
+
+def recommend_movies(model, movie_id, n_recommendations=10):
+    model.to(device)
+
+    model.eval()
+    with torch.no_grad():
+        target_movie_factors = model.movie_factors(torch.tensor(movie_to_index[movie_id]))
+        similarities = torch.matmul(model.movie_factors.weight, target_movie_factors)
+        top_n_similar = torch.topk(similarities, n_recommendations+1)
+        recommended_movies = [movies[idx] for idx in top_n_similar.indices[1:].numpy()]
+    return recommended_movies
+
+# Initialize the model
+n_users = len(users)
+n_movies = len(movies)
+model = RecommenderSystem(n_users, n_movies)
+
+# Train the model
+print(f"{datetime.now()} Starting training...")
+train_model(model, train_data, val_data)
+
+# Recommend movies based on a specific movie
+movie_id = 1  # Toy Story
+recommended_movies = recommend_movies(model, movie_id)
+print("Recommended movies based on Toy Story:", recommended_movies)
+
